739b1e48f2df0c647184153d0b091cf3a3ead284
taoshuaishuai <taoshuaishuai@doublefs.com>
2021-01-30 13:20:08 +0800

    1. add logger sdk and format output log 2. configure accessibility_service_config.xml for enable MallAccessibilityService

---
+++ app/src/main/java/com/tyyod/jdmallaccessibility/MallAccessibilityApplication.kt
@@ -0,0 +1,28 @@
+package com.tyyod.jdmallaccessibility
+
+import android.app.Application
+import com.orhanobut.logger.AndroidLogAdapter
+import com.orhanobut.logger.FormatStrategy
+import com.orhanobut.logger.Logger
+import com.orhanobut.logger.PrettyFormatStrategy
+
+class JDMallAccessibilityApplication: Application() {
+    override fun onCreate() {
+        super.onCreate()
+        initLogger()
+    }
+
+    private fun initLogger() {
+        val formatStrategy: FormatStrategy = PrettyFormatStrategy.newBuilder()
+            .showThreadInfo(false) // (Optional) Whether to show thread info or not. Default true
+            .methodCount(0)         // (Optional) How many method line to show. Default 2
+            .tag("JDMallAccessibility") // (Optional) Global tag for every log. Default PRETTY_LOGGER
+            .build()
+
+        Logger.addLogAdapter(object : AndroidLogAdapter(formatStrategy) {
+            override fun isLoggable(priority: Int, tag: String?): Boolean {
+                return BuildConfig.DEBUG
+            }
+        })
+    }
+}
--- app/src/main/java/com/tyyod/jdmallaccessibility/service/MallAccessibilityService.kt
+++ app/src/main/java/com/tyyod/jdmallaccessibility/service/MallAccessibilityService.kt
@@ -2,18 +2,49 @@ package com.tyyod.jdmallaccessibility.service
 
 import android.accessibilityservice.AccessibilityService
 import android.view.accessibility.AccessibilityEvent
+import android.view.accessibility.AccessibilityEvent.*
+import com.orhanobut.logger.Logger
 
 class MallAccessibilityService: AccessibilityService() {
 
     override fun onServiceConnected() {
         super.onServiceConnected()
+        Logger.d("---> MallAccessibilityService#onServiceConnected()")
     }
 
-    override fun onAccessibilityEvent(p0: AccessibilityEvent?) {
+    override fun onAccessibilityEvent(event: AccessibilityEvent?) {
+        if (event == null) {
+            return
+        }
 
+        var eventTypeStr = when (event.eventType) {
+            TYPE_VIEW_CLICKED -> "TYPE_VIEW_CLICKED"
+            TYPE_VIEW_LONG_CLICKED -> "TYPE_VIEW_LONG_CLICKED"
+            TYPE_VIEW_SELECTED -> "TYPE_VIEW_SELECTED"
+            TYPE_VIEW_FOCUSED -> "TYPE_VIEW_FOCUSED"
+            TYPE_VIEW_TEXT_CHANGED -> "TYPE_VIEW_TEXT_CHANGED"
+            TYPE_VIEW_TEXT_SELECTION_CHANGED -> "TYPE_VIEW_TEXT_SELECTION_CHANGED"
+            TYPE_VIEW_TEXT_TRAVERSED_AT_MOVEMENT_GRANULARITY -> "TYPE_VIEW_TEXT_TRAVERSED_AT_MOVEMENT_GRANULARITY"
+            TYPE_VIEW_SCROLLED -> "TYPE_VIEW_SCROLLED"
+            TYPE_WINDOW_STATE_CHANGED -> "TYPE_WINDOW_STATE_CHANGED"
+            TYPE_WINDOW_CONTENT_CHANGED -> "TYPE_WINDOW_CONTENT_CHANGED"
+            TYPE_WINDOWS_CHANGED -> "TYPE_WINDOWS_CHANGED"
+            TYPE_NOTIFICATION_STATE_CHANGED -> "TYPE_NOTIFICATION_STATE_CHANGED"
+            TYPE_VIEW_HOVER_ENTER -> "TYPE_VIEW_HOVER_ENTER"
+            TYPE_VIEW_HOVER_EXIT -> "TYPE_VIEW_HOVER_EXIT"
+            TYPE_TOUCH_INTERACTION_START -> "TYPE_TOUCH_INTERACTION_START"
+            TYPE_TOUCH_INTERACTION_END -> "TYPE_TOUCH_INTERACTION_END"
+            TYPE_TOUCH_EXPLORATION_GESTURE_START -> "TYPE_TOUCH_EXPLORATION_GESTURE_START"
+            TYPE_TOUCH_EXPLORATION_GESTURE_END -> "TYPE_TOUCH_EXPLORATION_GESTURE_END"
+            TYPE_GESTURE_DETECTION_START -> "TYPE_GESTURE_DETECTION_START"
+            TYPE_GESTURE_DETECTION_END -> "TYPE_GESTURE_DETECTION_END"
+            TYPE_ANNOUNCEMENT -> "TYPE_ANNOUNCEMENT"
+            else -> "Unknow"
+        }
+        Logger.d("onAccessibilityEvent: $eventTypeStr  EventId: ${event.eventType}")
     }
 
     override fun onInterrupt() {
-
+        Logger.d("---> MallAccessibilityService#onInterrupt()")
     }
 }
